a51dcf269b3089bf44bbbc2e69ed906af2c47c22
André Pires <andrebeckpires@gmail.com>
2020-05-07 00:01:24 -0300

    Teste de valor nulo

--- src/pages/NewCase/index.js
+++ src/pages/NewCase/index.js
@@ -18,7 +18,9 @@ export default function NewCase() {
 
     async function handleNewCase(e) {
         e.preventDefault();
-
+        if(value.isEmpty()){
+            value = null;
+        }
         const data = {
             title,
             description,
